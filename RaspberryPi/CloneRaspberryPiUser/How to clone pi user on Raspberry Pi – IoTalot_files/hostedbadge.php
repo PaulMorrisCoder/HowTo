8418de60a1bb8e65abbf3ac797364cc2e82a5f11
Paul Morris <pmorris@betfred.com>
2018-02-20 09:03:21 +0000

    How to clone raspberry pi user

---
+++ RaspberryPi/CloneRaspberryPiUser/How to clone pi user on Raspberry Pi – IoTalot_files/hostedbadge.php
@@ -0,0 +1,11 @@
+document.write('<su:badge layout="1"  location="https://iotalot.com/2017/02/28/how-to-clone-pi-user-on-raspberry-pi/"></su:badge>');
+document.write(" \
+<script type=\"text/javascript\"> \
+  (function() { \
+    var li = document.createElement('script'); li.type = 'text/javascript'; li.async = true; \
+    li.src = window.location.protocol + '//platform.stumbleupon.com/1/widgets.js'; \
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(li, s); \
+  })(); \
+</script>");
+
+
